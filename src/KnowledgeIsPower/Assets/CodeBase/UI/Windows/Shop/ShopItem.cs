4c55d2a2a8687abfb387770269299035b8dae1de
SweeTSweeP <Ruslan.Lomaka@Gmail.com>
2021-11-10 22:10:32 +0200

    Setup project

---
+++ src/KnowledgeIsPower/Assets/CodeBase/UI/Windows/Shop/ShopItem.cs
@@ -0,0 +1,40 @@
+﻿using CodeBase.Infrastructure.AssetManagement;
+using CodeBase.Services.IAP;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeBase.UI.Windows.Shop
+{
+  public class ShopItem : MonoBehaviour
+  {
+    public Button BuyItemButton;
+    public TextMeshProUGUI PriceText;
+    public TextMeshProUGUI SkullsQuantityText;
+    public TextMeshProUGUI AvailablePurchasesLeftText;
+    public Image Icon;
+
+    private ProductDescription _productDescription;
+    private IIAPService _iapService;
+    private IAssetProvider _assets;
+
+    public void Construct(IIAPService iapService, IAssetProvider assets, ProductDescription productDescription)
+    {
+      _productDescription = productDescription;
+      _iapService = iapService;
+      _assets = assets;
+    }
+
+    public async void Initialize()
+    {
+      BuyItemButton.onClick.AddListener(OnBuyItemButtonPressed);
+      PriceText.text = _productDescription.Config.Price;
+      SkullsQuantityText.text = _productDescription.Config.Quantity.ToString();
+      AvailablePurchasesLeftText.text = _productDescription.AvailablePurchasesLeft.ToString();
+      Icon.sprite = await _assets.Load<Sprite>(_productDescription.Config.Icon);
+    }
+
+    private void OnBuyItemButtonPressed() => 
+      _iapService.StartPurchase(_productDescription.Id);
+  }
+}
---
+++ src/KnowledgeIsPower/Assets/CodeBase/UI/Windows/Shop/ShopItemsContainer.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeBase.Infrastructure.AssetManagement;
+using CodeBase.Services.IAP;
+using CodeBase.Services.PersistentProgress;
+using UnityEngine;
+
+namespace CodeBase.UI.Windows.Shop
+{
+  public class ShopItemsContainer : MonoBehaviour
+  {
+    private const string ShopItemPath = "ShopItem";
+    
+    public GameObject[] ShopUnavailableObjects;
+    public Transform Parent;
+    private IIAPService _iapService;
+    private IPersistentProgressService _progressService;
+    private IAssetProvider _assets;
+    private readonly List<GameObject> _shopItems = new List<GameObject>();
+
+    public void Construct(IIAPService iapService, IPersistentProgressService progressService, IAssetProvider assets)
+    {
+      _iapService = iapService;
+      _progressService = progressService;
+      _assets = assets;
+    }
+
+    public void Initialize() => 
+      RefreshAvailableItems();
+
+    public void Subscribe()
+    {
+      _iapService.Initialized += RefreshAvailableItems;
+      _progressService.Progress.PurchaseData.Changed += RefreshAvailableItems;
+    }
+
+    public void Cleanup()
+    {
+      _iapService.Initialized -= RefreshAvailableItems;
+      _progressService.Progress.PurchaseData.Changed -= RefreshAvailableItems;
+    }
+
+    private async void RefreshAvailableItems()
+    {
+      UpdateShopUnavailableObjects();
+
+      if (!_iapService.IsInitialized)
+        return;
+      
+      ClearShopItems();
+
+      await FillShopItems();
+    }
+
+    private async Task FillShopItems()
+    {
+      foreach (ProductDescription productDescription in _iapService.Products())
+      {
+        GameObject shopItemObject = await _assets.Instantiate(ShopItemPath, Parent);
+
+        ShopItem shopItem = shopItemObject.GetComponent<ShopItem>();
+        shopItem.Construct(_iapService, _assets, productDescription);
+        shopItem.Initialize();
+
+        _shopItems.Add(shopItemObject);
+      }
+    }
+
+    private void ClearShopItems()
+    {
+      foreach (GameObject shopItem in _shopItems)
+        Destroy(shopItem);
+
+      _shopItems.Clear();
+    }
+
+    private void UpdateShopUnavailableObjects()
+    {
+      foreach (GameObject unavailableObject in ShopUnavailableObjects)
+        unavailableObject.SetActive(!_iapService.IsInitialized);
+    }
+  }
+}
