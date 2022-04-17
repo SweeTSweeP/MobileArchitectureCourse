ddb3823b2dac35a2ffbf3ff084e436755c38d847
Kraitshow <ruslan.lomaka@gmail.com>
2022-04-17 16:12:04 +0300

    Implement sixth lesson (enemy attack)

---
+++ src/KnowledgeIsPower/Assets/MainProject/Scripts/Enemy/Attack.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using CodeBase.Enemy;
+using MainProject.Scripts.Infrastructure.Factory;
+using MainProject.Scripts.Infrastructure.Services;
+using UnityEngine;
+
+namespace MainProject.Scripts.Enemy
+{
+    [RequireComponent(typeof(EnemyAnimator))]
+    public class Attack : MonoBehaviour
+    {
+        [SerializeField] private EnemyAnimator animator;
+
+        [SerializeField] private float attackCooldown = 3f;
+        [SerializeField] private float cleavage = 0.5f;
+        [SerializeField] private float effectiveDistance = 0.5f;
+
+        private IGameFactory _gameFactory;
+        private Transform _heroTransform;
+        private float _attackCooldown;
+        private bool _isAttacking;
+        private int _layerMask;
+        private Collider[] _hits = new Collider[1];
+        private bool _attackIsActive;
+
+        private void Awake()
+        {
+            _gameFactory = AllServices.Container.Single<IGameFactory>();
+            _gameFactory.HeroCreated += OnHeroCreated;
+
+            _layerMask = 1 << LayerMask.NameToLayer("Player");
+        }
+
+        private void Update()
+        {
+            UpdateCooldown();
+
+            if (CanAttack()) 
+                StartAttack();
+        }
+
+        private void OnAttack()
+        {
+            if (Hit(out Collider hit))
+            {
+                PhysicsDebug.DrawDebug(StartPoint(), cleavage, 1);
+            }
+        }
+
+        private void OnAttackEnded()
+        {
+            _attackCooldown = attackCooldown;
+            _isAttacking = false;
+        }
+
+        public void EnableAttack() => 
+            _attackIsActive = true;
+
+        public void DisableAttack() => 
+            _attackIsActive = false;
+
+        private bool Hit(out Collider hit)
+        {
+            var hitsCount = Physics.OverlapSphereNonAlloc(StartPoint(), cleavage, _hits, _layerMask);
+
+            hit = _hits.FirstOrDefault();
+            
+            return hitsCount > 0;
+        }
+
+        private Vector3 StartPoint() => 
+            new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z) + transform.forward * effectiveDistance;
+
+        private void StartAttack()
+        {
+            transform.LookAt(_heroTransform);
+            animator.PlayAttack();
+
+            _isAttacking = true;
+        }
+
+        private bool CanAttack() => 
+            _attackIsActive && !_isAttacking && CooldownIsUp();
+
+        private void UpdateCooldown()
+        {
+            if (!CooldownIsUp())
+                _attackCooldown -= Time.deltaTime;
+        }
+
+        private bool CooldownIsUp()
+        {
+            return _attackCooldown <= 0;
+        }
+
+        private void OnHeroCreated() => 
+            _heroTransform = _gameFactory.HeroGameObject.transform;
+    }
+}
---
+++ src/KnowledgeIsPower/Assets/MainProject/Scripts/Enemy/CheckAttackRange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace MainProject.Scripts.Enemy
+{
+    [RequireComponent(typeof(Attack))]
+    public class CheckAttackRange : MonoBehaviour
+    {
+        [SerializeField] private Attack attack;
+        [SerializeField] private TriggerObserver triggerObserver;
+
+        private void Start()
+        {
+            triggerObserver.TriggerEnter += TriggerEnter;
+            triggerObserver.TriggerExit += TriggerExit;
+            
+            attack.DisableAttack();
+        }
+
+        private void TriggerExit(Collider obj)
+        {
+            attack.DisableAttack();
+        }
+
+        private void TriggerEnter(Collider obj)
+        {
+            attack.EnableAttack();
+        }
+    }
+}
